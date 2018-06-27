138ccb07c7cac80be22ec4c820e6f65fbf47648c
ronaldo faustino <ohnirucse@msn.com>
2018-06-27 13:29:03 -0300

    For4Final

--- mark-tasks/features/support/env.rb
+++ mark-tasks/features/support/env.rb
@@ -3,8 +3,46 @@ require 'capybara'
 require 'capybara/cucumber'
 require 'site_prism'
 require 'faker'
+require 'capybara/poltergeist'
 
 $env = ENV['BROWSER']
+#$browser = ENV['BROWSER']
+
+
+Capybara.register_driver :selenium do |app|
+   if BROWSER.eql?('remote')
+      Capybara::
+              Selenium::
+              Driver.new(app, browser: :remote,
+                       url: 'http://selenium-hub:4444/wd/hub',
+                         desired_capabilities: Selenium::
+                                 WebDriver::Remote::Capabilities.chrome(
+                             'chromeOptions' => { args: %w[window-size=1366,768] }
+                         ))
+    elsif BROWSER.eql?('firefox')
+      Capybara::
+              Selenium::
+              Driver.new(app, browser: :firefox)
+    else
+      Capybara::
+              Selenium::
+              Driver.new(app, browser: :chrome,
+                         desired_capabilities: Selenium::
+                                 WebDriver::Remote::Capabilities.chrome(
+                             'chromeOptions' => { args: ['--headless disable-gpu
+                          --window-size=1024,768 --no-sandbox'] }
+                         ))
+    end
+  end
+
+
+#$headless = ENV['HEADLESS']
+
+#if $headless
+#  Capybara.register_driver :selenium do |app|
+#    Capybara::Poltergeist::Driver.new(app, js_errors: false)
+#    end
+#end
 
 #Capybara.configure do |config|
 #    config.default_driver = :selenium
